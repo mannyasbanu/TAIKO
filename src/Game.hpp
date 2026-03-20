2854ecb6e00af394e1c20cfb1cea78288d9faf87
mannyasbanu <asbanu.manny@gmail.com>
2026-03-20 21:27:34 +1030

    game header

---
+++ src/Game.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <vector>
+#include "Note.hpp"
+#include "BeatMap.hpp"
+// Manny STL ^
+
+class Game {
+public:
+  Game();
+  void run();
+private:
+  void processEvents(); // process input
+  void update(float dt); // update game state (dt is time since last frame in seconds)
+  void render(); // render to screen
+  void handleInput(NoteType type);
+
+  sf::RenderWindow window;
+  sf::Music music;
+  BeatMap beatMap;
+
+  float songTimeMs = 0.0f;
+  int score = 0;
+  int combo = 0;
+
+  // visuals
+  std::string lastResult = "";
+  float resultTimer = 0.0f;
+
+  // class level constants
+  static constexpr float NOTE_SPEED = 0.f; // pixels per ms
+  static constexpr float HIT_ZONE_X = 200.0f; // x position of hit zone
+  static constexpr float NOTE_RADIUS = 30.0f;
+};
